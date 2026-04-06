7453fb1153197ab595d7a6c75d1d5b4dc014bc7a
Deepak Kalal <kdeepak.cd@gmail.com>
2026-04-06 15:33:12 +0530

    Update API_BASE_URL to new endpoint

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,7 +1,7 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://851725605576.realhandsonlabs.net"
+const API_BASE_URL = "http://54.234.74.24:84"
 
-// const API_BASE_URL = "http://98.92.112.243:84"
+// const API_BASE_URL = "http://54.234.74.24:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
